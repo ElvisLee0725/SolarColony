2126d14f0fa587539d051d2308f6f6fe93fe4dbf
Elvis Lee <elvislee0725@gmail.com>
2015-05-30 01:08:52 -0700

    do it

---
+++ SolarColony/Resources/images/CS 526 Project/testexport.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sample_bg" tilewidth="76" tileheight="64">
+ <tileoffset x="-5" y="0"/>
+ <image source="Tile samples/sample_bg.png" width="1216" height="1024"/>
+</tileset>
